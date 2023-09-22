f7995bea8aa078c8cc4434e385f2ca9a9b8cf35f
inescurso <73557971+inescurso@users.noreply.github.com>
2023-09-22 01:12:00 -0300

    Delete cursos.js

--- cursos.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("bo_cursos").addEventListener('click', function ()  {
-
-    document.getElementById('estudios').style.display= ' none ';
-    document.getElementById('cursos').style.display= ' block ';
-    document.getElementById('experiencia').style.display= ' none ';
-    document.getElementById('video').style.display= ' none ';
-    
- });
